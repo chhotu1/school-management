96f1b584b000dfa37061904159222badafd02f27
chhotu1 <chhotukumarsow@gmail.com>
2022-05-29 22:29:49 +0530

    fee module

--- pages/admin/employee/add.js
+++ pages/admin/employee/add.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { inputChange } from '../../../src/redux/reducers/UserReducer';
 import {
     Grid,
 } from "@mui/material";
@@ -8,18 +7,16 @@ import BaseCard from "../../../src/admin/components/baseCard/BaseCard";
 import EmployeeForm from '../../../src/admin/components/employee/employee-form';
 import withAuth from '../../../src/Share/withAuth';
 const Add = () => {
-    const dispatch = useDispatch();
-    const user = useSelector((state) => state.user.user);
+
     const handleChange = async (e) => {
         const { name, value } = e.target;
-        await dispatch(inputChange({ [name]: value }));
     }
     return (
         <Grid container spacing={0}>
             <Grid item xs={12} lg={12}>
                 <BaseCard title="New Employee" backArrow="return">
                     <form>
-                        <EmployeeForm handleChange={handleChange} user={user} />
+                        <EmployeeForm handleChange={handleChange}/>
                         <div className="col-auto">
                             <button type="submit" className="btn btn-primary mb-3">Add new employee</button>
                         </div>
